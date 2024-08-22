97eb808107bfc8566d21b7162718a57a37c2f8d2
Umman Babayev <ummanbabayev1@gmail.com>
2024-08-22 19:59:19 +0400

    Initial Commit

---
+++ ToDoAppSwiftUI/ToDoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppSwiftUIApp.swift
+//  ToDoAppSwiftUI
+//
+//  Created by Umman on 22.08.24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
